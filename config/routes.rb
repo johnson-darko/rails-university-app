5f161c2c36509a8eeb94da09a60a1a904bbf2dc9
johnson-darko <johnsondarko365@gmail.com>
2022-09-07 18:14:08 +0200

    styled

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :tasks
   end
   devise_for :users
-  root to: "pages#home"
+  root to: "projects#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
